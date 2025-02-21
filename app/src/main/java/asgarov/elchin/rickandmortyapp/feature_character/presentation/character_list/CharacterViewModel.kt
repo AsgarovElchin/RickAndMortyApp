8995e8df4bccc401413818f595a66c98dabe527a
AsgarovElchin <elcinaskerovki@gmail.com>
2025-02-21 17:55:53 +0400

    add CharacterViewModel

---
+++ app/src/main/java/asgarov/elchin/rickandmortyapp/feature_character/presentation/character_list/CharacterViewModel.kt
@@ -0,0 +1,19 @@
+package asgarov.elchin.rickandmortyapp.feature_character.presentation.character_list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import asgarov.elchin.rickandmortyapp.feature_character.domain.model.Character
+import asgarov.elchin.rickandmortyapp.feature_character.domain.repository.CharacterRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CharacterViewModel @Inject constructor(
+    private val repository: CharacterRepository
+
+):ViewModel() {
+
+    val charactersFlow: Flow<PagingData<Character>> =
+        repository.getAllCharacters().cachedIn(viewModelScope)
+}
