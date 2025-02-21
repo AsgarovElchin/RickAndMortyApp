a13d4ab22b1fc22e132a072ef6bec9f8a2279e11
AsgarovElchin <elcinaskerovki@gmail.com>
2025-02-21 18:56:32 +0400

    DI module setup done

---
+++ app/src/main/java/asgarov/elchin/rickandmortyapp/RickAndMortyApplication.kt
@@ -0,0 +1,8 @@
+package asgarov.elchin.rickandmortyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickAndMortyApplication:Application() {
+}
