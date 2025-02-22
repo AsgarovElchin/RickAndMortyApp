619ca707fc91b7469c03d89fcc31f997f27e1f47
AsgarovElchin <elcinaskerovki@gmail.com>
2025-02-22 19:08:51 +0400

    add getCharacterById function in API interface

--- app/src/main/java/asgarov/elchin/rickandmortyapp/feature_character/data/remote/CharacterApi.kt
+++ app/src/main/java/asgarov/elchin/rickandmortyapp/feature_character/data/remote/CharacterApi.kt
@@ -1,11 +1,16 @@
 package asgarov.elchin.rickandmortyapp.feature_character.data.remote
 
+import asgarov.elchin.rickandmortyapp.feature_character.data.remote.dto.CharacterDto
 import asgarov.elchin.rickandmortyapp.feature_character.data.remote.dto.CharacterListDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterApi {
 
     @GET("character")
     suspend fun getAllCharacters(@Query("page") page: Int): CharacterListDto
+
+    @GET("character/{id}")
+    suspend fun getCharacterById(@Path("id")id:Int):CharacterDto
 }
