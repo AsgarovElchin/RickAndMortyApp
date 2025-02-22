d7d5e71333a78762a776d2cb0c0698f62efd4959
AsgarovElchin <elcinaskerovki@gmail.com>
2025-02-22 19:59:05 +0400

    add getCharacterById function to CharacterRepository interface and CharacterRepositoryImpl class

--- app/src/main/java/asgarov/elchin/rickandmortyapp/feature_character/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/asgarov/elchin/rickandmortyapp/feature_character/data/repository/CharacterRepositoryImpl.kt
@@ -3,11 +3,18 @@ package asgarov.elchin.rickandmortyapp.feature_character.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import asgarov.elchin.rickandmortyapp.core.utils.Resource
+import asgarov.elchin.rickandmortyapp.feature_character.data.mapper.toCharacter
 import asgarov.elchin.rickandmortyapp.feature_character.data.paging.CharacterPagingSource
 import asgarov.elchin.rickandmortyapp.feature_character.data.remote.CharacterApi
 import asgarov.elchin.rickandmortyapp.feature_character.domain.model.Character
 import asgarov.elchin.rickandmortyapp.feature_character.domain.repository.CharacterRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import okio.IOException
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class CharacterRepositoryImpl @Inject constructor(
@@ -24,5 +31,20 @@ class CharacterRepositoryImpl @Inject constructor(
             pagingSourceFactory = {CharacterPagingSource(api)}
         ).flow
 
+    }
+
+    override fun getCharacterById(id: Int): Flow<Resource<Character>> {
+        return flow{
+            try {
+                emit(Resource.Loading())
+                val characterDetails = api.getCharacterById(id).toCharacter()
+                emit(Resource.Success(characterDetails))
+
+            }catch (e:HttpException){
+                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+            }catch (e:IOException){
+                emit(Resource.Error(e.localizedMessage ?: "Couldn't reach server. Check your internet connection"))
+            }
+        }.flowOn(Dispatchers.IO)
     }
 }
--- app/src/main/java/asgarov/elchin/rickandmortyapp/feature_character/domain/repository/CharacterRepository.kt
+++ app/src/main/java/asgarov/elchin/rickandmortyapp/feature_character/domain/repository/CharacterRepository.kt
@@ -1,10 +1,13 @@
 package asgarov.elchin.rickandmortyapp.feature_character.domain.repository
 
 import androidx.paging.PagingData
+import asgarov.elchin.rickandmortyapp.core.utils.Resource
 import asgarov.elchin.rickandmortyapp.feature_character.domain.model.Character
 import kotlinx.coroutines.flow.Flow
 
 interface CharacterRepository {
 
     fun getAllCharacters(): Flow<PagingData<Character>>
+
+    fun getCharacterById(id: Int): Flow<Resource<Character>>
 }
